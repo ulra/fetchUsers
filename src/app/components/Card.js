eff515364825ae04cf51591b510de467ba9db20e
ulra <ra.do.nu20@gmail.com>
2023-03-29 17:11:41 -0400

    key

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -11,8 +11,8 @@ const Card = (props) => {
         <div className="containe">
             <div className="row">
             {props.users.map(user => (
-               <div className="col-sm-12 col-md-4 ">
-               <div className="card mb-3 m-2 mx-auto"  key={user.id}>
+               <div key={user.id} className="col-sm-12 col-md-4 ">
+               <div className="card mb-3 m-2 mx-auto"  >
   <h5 className="card-header">{user.first_name} {user.last_name}</h5>
   <div className="card-body">
   </div>
